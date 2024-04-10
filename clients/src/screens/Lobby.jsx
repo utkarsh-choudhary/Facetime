5a0dca8b98e1976184f5d2d4d0af4c104342c341
utkarsh-choudhary <utkarshchoudhary734@gmail.com>
2024-04-11 00:20:23 +0530

    added client

---
+++ clients/src/screens/Lobby.jsx
@@ -0,0 +1,62 @@
+import React, { useState,useCallback,useEffect } from "react";
+import {useNavigate} from "react-router-dom"
+import { useSocket } from "../context/SocketProvider";
+
+const LobbyScreen = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const socket=useSocket();
+  const navigate=useNavigate();
+
+  const handleSubmitForm=useCallback((e)=>{
+    e.preventDefault();
+    socket.emit("room:join",{email,room});
+    
+  },[email,room,socket]);
+
+
+  const handleJoinRoom=useCallback((data)=>{
+    const {email,room}=data;
+    navigate(`/room/${room}`)
+  },[navigate])
+
+  useEffect(()=>{
+    socket.on("room:join",handleJoinRoom);
+    return ()=> {
+      socket.off("room:join",handleJoinRoom);
+    }
+  },[socket,handleJoinRoom])
+
+
+  return (
+    <div className="container">
+      <h1>Lobby</h1>
+      <form onSubmit={handleSubmitForm}>
+        <label htmlFor="email"> Email ID  </label>
+        <input
+        className="email"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br/>
+        <label htmlFor="room">Room Number  </label>
+        <input
+          type="text"
+          id="room"
+          className="room"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <br />
+        <br />
+        <button className="btn">Join</button>
+      </form>
+    </div>
+  );
+};
+
+export default LobbyScreen;
